b687e13055e294f39bfe5670b1f4a76f18cc08f7
Sergei Korolev <dromgard@yandex.ru>
2023-03-12 00:01:28 +0300

    feat: Фон готов

--- amoCRM/welbex/src/components/Main/Main.js
+++ amoCRM/welbex/src/components/Main/Main.js
@@ -75,6 +75,113 @@ function Main() {
           title="Получить консультацию"
         >Получить консультацию</button>
       </div>
+      <svg className="main__background-red-big" width="117" height="117" viewBox="0 0 117 117" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_bd_0_13)">
+          <circle cx="55.5" cy="52.5" r="41.5" fill="url(#paint0_radial_0_13)" fill-opacity="0.8" shape-rendering="crispEdges" />
+        </g>
+        <defs>
+          <filter id="filter0_bd_0_13" x="0" y="0" width="117" height="117" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feGaussianBlur in="BackgroundImageFix" stdDeviation="2" />
+            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_0_13" />
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+            <feMorphology radius="10" operator="erode" in="SourceAlpha" result="effect2_dropShadow_0_13" />
+            <feOffset dx="3" dy="6" />
+            <feGaussianBlur stdDeviation="13.5" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix type="matrix" values="0 0 0 0 0.87451 0 0 0 0 0.490196 0 0 0 0 0.490196 0 0 0 0.25 0" />
+            <feBlend mode="normal" in2="effect1_backgroundBlur_0_13" result="effect2_dropShadow_0_13" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_0_13" result="shape" />
+          </filter>
+          <radialGradient id="paint0_radial_0_13" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(36.6175 32.995) rotate(51.0412) scale(68.3129)">
+            <stop stop-color="#FFD0D0" stop-opacity="0.67" />
+            <stop offset="0.596678" stop-color="#D45D5D" stop-opacity="0.33" />
+            <stop offset="0.785899" stop-color="#A73131" stop-opacity="0.31" />
+            <stop offset="1" stop-color="#821919" stop-opacity="0.31" />
+          </radialGradient>
+        </defs>
+      </svg>
+      <svg className="main__background-red-small" width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_bdf_0_12)">
+          <circle cx="28" cy="25" r="14" fill="url(#paint0_radial_0_12)" fill-opacity="0.8" shape-rendering="crispEdges" />
+        </g>
+        <defs>
+          <filter id="filter0_bdf_0_12" x="0" y="0" width="62" height="62" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feGaussianBlur in="BackgroundImageFix" stdDeviation="2" />
+            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_0_12" />
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+            <feMorphology radius="10" operator="erode" in="SourceAlpha" result="effect2_dropShadow_0_12" />
+            <feOffset dx="3" dy="6" />
+            <feGaussianBlur stdDeviation="13.5" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix type="matrix" values="0 0 0 0 0.87451 0 0 0 0 0.490196 0 0 0 0 0.490196 0 0 0 0.25 0" />
+            <feBlend mode="normal" in2="effect1_backgroundBlur_0_12" result="effect2_dropShadow_0_12" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_0_12" result="shape" />
+            <feGaussianBlur stdDeviation="2.5" result="effect3_foregroundBlur_0_12" />
+          </filter>
+          <radialGradient id="paint0_radial_0_12" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(21.63 18.42) rotate(51.0412) scale(23.0453)">
+            <stop stop-color="#FFD0D0" stop-opacity="0.6" />
+            <stop offset="0.596678" stop-color="#D45D5D" stop-opacity="0.31" />
+            <stop offset="0.785899" stop-color="#A73131" stop-opacity="0.33" />
+            <stop offset="1" stop-color="#821919" stop-opacity="0.29" />
+          </radialGradient>
+        </defs>
+      </svg>
+      <svg className="main__background-purple-mobile" width="38" height="74" viewBox="0 0 38 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_bdf_0_80)">
+          <circle cx="34" cy="31" r="20" fill="url(#paint0_radial_0_80)" fill-opacity="0.8" shape-rendering="crispEdges" />
+        </g>
+        <defs>
+          <filter id="filter0_bdf_0_80" x="0" y="0" width="74" height="74" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feGaussianBlur in="BackgroundImageFix" stdDeviation="2" />
+            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_0_80" />
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+            <feMorphology radius="10" operator="erode" in="SourceAlpha" result="effect2_dropShadow_0_80" />
+            <feOffset dx="3" dy="6" />
+            <feGaussianBlur stdDeviation="13.5" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix type="matrix" values="0 0 0 0 0.694118 0 0 0 0 0.462745 0 0 0 0 0.85098 0 0 0 0.25 0" />
+            <feBlend mode="normal" in2="effect1_backgroundBlur_0_80" result="effect2_dropShadow_0_80" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_0_80" result="shape" />
+            <feGaussianBlur stdDeviation="5" result="effect3_foregroundBlur_0_80" />
+          </filter>
+          <radialGradient id="paint0_radial_0_80" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(24.9 21.6) rotate(51.0412) scale(32.9219)">
+            <stop stop-color="#ECD0FF" stop-opacity="0.8" />
+            <stop offset="0.596678" stop-color="#A15DCF" stop-opacity="0.3" />
+            <stop offset="0.785899" stop-color="#7631A4" stop-opacity="0.26" />
+            <stop offset="1" stop-color="#561880" stop-opacity="0.29" />
+          </radialGradient>
+        </defs>
+      </svg>
+      <svg className="main__background-red-mobile" width="77" height="54" viewBox="0 0 77 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_bd_0_84)">
+          <circle cx="35.5" cy="32.5" r="21.5" fill="url(#paint0_radial_0_84)" fill-opacity="0.8" shape-rendering="crispEdges" />
+        </g>
+        <defs>
+          <filter id="filter0_bd_0_84" x="0" y="0" width="77" height="77" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feGaussianBlur in="BackgroundImageFix" stdDeviation="2" />
+            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_0_84" />
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+            <feMorphology radius="10" operator="erode" in="SourceAlpha" result="effect2_dropShadow_0_84" />
+            <feOffset dx="3" dy="6" />
+            <feGaussianBlur stdDeviation="13.5" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix type="matrix" values="0 0 0 0 0.87451 0 0 0 0 0.490196 0 0 0 0 0.490196 0 0 0 0.25 0" />
+            <feBlend mode="normal" in2="effect1_backgroundBlur_0_84" result="effect2_dropShadow_0_84" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_0_84" result="shape" />
+          </filter>
+          <radialGradient id="paint0_radial_0_84" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(25.7175 22.395) rotate(51.0412) scale(35.391)">
+            <stop stop-color="#FFD0D0" stop-opacity="0.67" />
+            <stop offset="0.596678" stop-color="#D45D5D" stop-opacity="0.33" />
+            <stop offset="0.785899" stop-color="#A73131" stop-opacity="0.31" />
+            <stop offset="1" stop-color="#821919" stop-opacity="0.31" />
+          </radialGradient>
+        </defs>
+      </svg>
+      <div className="main__background-noise"></div>
     </main>
   )
 }
